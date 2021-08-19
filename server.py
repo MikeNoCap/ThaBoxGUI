1fb825805410f12e6ddad9a6025e8df3f486fb56
Mike <thorvaldsenmikkel@gmail.com>
2021-08-19 19:57:54 +0200

    Initial

---
+++ server.py
@@ -0,0 +1,21 @@
+# Server
+import socketio
+from aiohttp import web
+from fastapi import FastAPI
+# Configure server app
+sio = socketio.Server()
+app = socketio.WSGIApp(sio)
+
+
+@sio.event
+def connect():
+    print("I'm connected!")
+
+@sio.event
+def connect_error(data):
+    print("The connection failed!")
+
+@sio.event
+def disconnect():
+    print("I'm disconnected!")
+    
