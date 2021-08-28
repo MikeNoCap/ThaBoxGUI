4c3bdd29dc081f63a9d7399d9c31aba0003b1885
Mike <thorvaldsenmikkel@gmail.com>
2021-08-28 18:31:32 +0200

    Cleaned up looks on clientside.

--- thabox/main.py
+++ thabox/main.py
@@ -3,9 +3,10 @@ import socketio
 import tkinter as tk
 from PIL import Image, ImageTk
 import asyncio
+import textwrap
 
-messages = ["\n" for i in range(10)]
-
+messages = []
+socket_thread = None
 
 
 sio = socketio.AsyncClient()
@@ -15,13 +16,15 @@ CONNECTED = False
 USERNAME = ""
 ROOM = ""
 
+EXIT = False
+
 root = tk.Tk()
-
+root.geometry("500x692")
 root.title("ThaBox")
-root.iconbitmap("icon.ico")
+root.iconbitmap("thabox\icon.ico")
 
 
-img = Image.open("ThaBox.png")
+img = Image.open("thabox\ThaBox.png")
 img = img.resize((500, 500))
 tkimage = ImageTk.PhotoImage(img)
 
@@ -41,7 +44,9 @@ def box_chat(username, room_name):
     
 
 async def on_closing():
-    await sio.disconnect()
+    global EXIT
+    EXIT = True
+    await asyncio.sleep(1.5)
     root.destroy()
 
 logo = tk.Label(image=tkimage, borderwidth=0)
@@ -51,31 +56,53 @@ input_frame = tk.Frame(root, background="black")
 
 
 
-room_label = tk.Label(input_frame, text="Room-name:", background="cyan", font="Haettenschweiler 14")
-room_entry = tk.Entry(input_frame, width=23)
+room_label = tk.Label(input_frame, text="Room-name:", background="cyan", font="Haettenschweiler 18")
+room_entry = tk.Entry(input_frame, font="Haettenschweiler 18")
 
-username_label = tk.Label(input_frame, text="Username:    ", background="magenta", font="Haettenschweiler 14")
-username_entry = tk.Entry(input_frame, width=23)
+username_label = tk.Label(input_frame, text="Username:    ", background="magenta", font="Haettenschweiler 18")
+username_entry = tk.Entry(input_frame, font="Haettenschweiler 18")
 
 
 error_message = tk.Label(root, text="\n", background="black", font="Courier 25")
 error_message.configure(foreground="red")
 
 
-@sio.event
+@sio.event # 19
 async def receive_message(data):
-    global ROOM, messages, USERNAME
+    global ROOM, messages, msgs, rows_used, message_boxes
     if data["room_name"] == ROOM:
-        prev_messages = messages.cget("text")
-        messages.configure(text=f"{prev_messages}{data['username']}: {data['message']}\n")
+        rows_used += len(data["message"].splitlines())
+        while rows_used > 14:
+            msg_box = message_boxes.pop(0)
+            text = msg_box[2].cget("text")
+            for i in msg_box:
+                i.destroy()
+            rows_used -= len(text.splitlines())
+        
+        
+        messages.append((data['username'], data['message']))
+        
+        msg_frame = tk.Frame(msgs, background="black")
+        author = tk.Label(msg_frame, background="magenta", foreground="black", text=data['username'], font="Haettenschweiler 18")
+        message = tk.Label(msg_frame, background="cyan", foreground="black", text=data['message'], font="Haettenschweiler 18")
+        author.grid(row=0, column=0, sticky="E", pady=5)
+        message.grid(row=0, column=1, sticky="W", padx=5, pady=5)
+        msg_frame.grid(row=len(messages), column=0)
 
+        message_boxes.append((msg_frame, author, message))
+        
 
 async def send_message(username, room_name, message):
-    await sio.emit("send_message", data={"username": username, "room_name": room_name, "message": message})
+    if (message_text := message.get()) == "":
+        return
+    if len(message_text) > 81:
+        return
+    await sio.emit("send_message", data={"username": username, "room_name": room_name, "message": "\n".join(textwrap.wrap(message_text, 27))})
+    message.delete(0, tk.END)
 
 
 async def chat_room(username, room_name):
-    global messages, message_entry
+    global messages, message_entry, msgs, rows_used, message_boxes
     globals().update(USERNAME=username)
     globals().update(ROOM=room_name)
 
@@ -83,21 +110,33 @@ async def chat_room(username, room_name):
     input_frame.destroy()
     join_button.destroy()
     error_message.destroy()
-
-    message_input = tk.Frame(root, background="black")
-    message_entry = tk.Entry(message_input)
-    loop = asyncio.new_event_loop()
-    message_send_button = tk.Button(message_input, text="Send", command=lambda: loop.run_until_complete(send_message(username, room_name, message_entry.get())))
-
-    messages = tk.Label(root, text="", background="black", foreground="cyan", font="Haettenschweiler 16")
     
-    messages.grid(row=0)
+    message_boxes = []
+    rows_used = 0
 
-    message_entry.grid(row=0, column=0)
+    message_input = tk.Frame(root, background="red")
+    message_entry = tk.Entry(message_input, font="Haettenschweiler 19", width=23)
+    loop = asyncio.new_event_loop()
+    message_send_button = tk.Button(message_input, font="Haettenschweiler 20", background="red",text="Send", command=lambda: loop.run_until_complete(send_message(username, room_name, message_entry)))
+
+
+    msgs = tk.Frame(root, background="black")
+    msgs.grid(row=0, column=0)
+
+    
+
+    
+    message_entry.grid(row=0, column=0, padx=10)
     message_send_button.grid(row=0, column=1)
     message_input.grid(row=1, column=0)
+    
+    
 
     while True:
+        global EXIT 
+        if EXIT:
+            await sio.disconnect()
+            return
         await asyncio.sleep(0.3)
 
 
@@ -129,25 +168,28 @@ def set_error_text(text, color="red"):
 
 
 def join_create():
+    global socket_thread
     username = username_entry.get()
     room_name = room_entry.get()
     if username == "":
         return set_error_text("Please enter a\nusername first")
     if len(username) < 3 or len(username) > 16:
+        username_entry.delete(0, tk.END)
         return set_error_text("Username should be\n3-16 characters")
     if len(room_name) == 0:
         return set_error_text("Room name can't\nbe empty")
     
-    threading.Thread(target=lambda: asyncio.run(connect_server(username, room_name))).start()
+    socket_thread = threading.Thread(target=lambda: asyncio.run(connect_server(username, room_name)))
+    socket_thread.start()
 
 join_button = tk.Button(root, text="Join/Create box", padx=70, pady=14, font="Haettenschweiler 21", background="#b40000", command=join_create)
 
 
-username_label.grid(row=1, column=2, ipady=5)#, sticky="n")
-username_entry.grid(row=1, column=3, ipady=7)#, sticky="n")
+username_label.grid(row=1, column=2, ipady=5, pady=5, sticky="e")#, sticky="n")
+username_entry.grid(row=1, column=3, ipady=7, pady=5)#, sticky="n")
 
-room_label.grid(row=1, column=4, ipady=5)#, sticky="w")
-room_entry.grid(row=1, column=5, ipady=7)#, sticky="w")
+room_label.grid(row=1, column=4, ipady=5, pady=5, sticky="e")#, sticky="w")
+room_entry.grid(row=1, column=5, ipady=7, pady=5)#, sticky="w")
 
 join_button.grid(row=2, column=0)
 
@@ -165,10 +207,14 @@ root.columnconfigure(0, weight=1)
 root.rowconfigure(0, weight=1)
 root.configure(background='black')
 root.configure(highlightthickness=0)
+root.resizable(False, False)
 root.attributes('-topmost', True)
 
 
 
 
+def start():
+    root.mainloop() 
 
-root.mainloop() # THREADING WILL SOLVE ALL OF MY PROBLEMS:
+if __name__ == '__main__':
+    start()
